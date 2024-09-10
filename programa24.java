56287a6cef7e01ddcbd1675062c7a2d0ef975c03
jhonatanaguilarpaja <holagringopoli3216@gmail.com>
2024-09-10 11:31:38 -0500

    Add files via upload

---
+++ programa24.java
@@ -0,0 +1,29 @@
+
+
+import java.util.*;
+public class programa24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        utiless plumono = new utiless();
+        utiless mota = new utiless();
+        utiless pizarra= new utiless();
+
+
+
+    }
+}
+
+class utiless{
+    String marcas,color,tamanio,modelo;
+    double precio;
+    double peso;
+    public void utilizar(){
+
+    }
+    public void manipular(){
+
+    }
+    public void comercializar (){
+
+    }
+}
