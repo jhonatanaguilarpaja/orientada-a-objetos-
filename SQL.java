49a451d0e7a8d61a9cc266d6ab48273b9a8fba41
jhonatanaguilarpaja <holagringopoli3216@gmail.com>
2024-11-04 18:19:23 -0500

    Add files via upload

---
+++ SQL.java
@@ -0,0 +1,77 @@
+public class SQL {
+
+
+
+    // nos sirve para introducir productos desde sql
+//INSERT INTO productos (proveedor,precio,cantidad,descripcion)
+   // VALUS('fernado',52.26,45,'gallina')
+
+
+
+
+
+    //seleccionamos los registros que cumplan la condiccion de cantidad
+    //SELECT*FROM productos WHERE cantidad >=10;
+
+
+
+
+
+    //ordenamos por cantidad en forma ascendente
+    //SELECT * FROM productos ORDER BY cantidad DESC;
+
+
+
+
+
+
+    //busquedas
+    //SELECT*FROM productos WHERE descripcion LIKE '%co%';
+
+
+
+
+
+
+    /*la siguiente sentencia modifica el registo cuyo id es igual al 1*/
+   // UPDATE productos SET descricion"azucár rubia",cantidad=100,proveedor="pomalca"WHERE id=1;
+
+
+
+
+
+
+    /* Eliminar cuyo id es igual al número que quieres eliminar */
+    //DELETE FROM productos WHERE id=8;
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
